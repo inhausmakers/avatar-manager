aa85885f7300d475923a001163762edebe12a9ea
Catalin Dogaru <catalin.dogaru@gmail.com>
2013-01-20 12:19:57 +0200

    Added the plugin file

---
+++ avatar-manager.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: Avatar Managaer
+Plugin URI: https://github.com/cdog/avatar-manager
+Description: Avatar Manager for WordPress is a sweet and simple plugin for storing avatars locally and more.
+Version: 1.0.0
+Author: Cătălin Dogaru
+Author URI: http://swarm.cs.pub.ro/~cdogaru/
+License: GPLv2 or later
+*/
+?>
+
+<?php
+/*
+Copyright © 2013 Cătălin Dogaru
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+?>
