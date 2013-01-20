61632a0ef32ff7bce2f509c0a94bf80df0cf57ea
Catalin Dogaru <catalin.dogaru@gmail.com>
2013-01-20 15:30:48 +0200

    Added setting fields

--- avatar-manager.php
+++ avatar-manager.php
@@ -33,7 +33,7 @@ define( 'AVATAR_MANAGER_VERSION', '1.0.0' );
 define( 'AVATAR_MANAGER_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
 /**
- * Sets up plugin defaults and makes Avatar Manager available for translation.
+ * Inits the plugin and makes Avatar Manager available for translation.
  *
  * @uses load_theme_textdomain() For translation/localization support.
  *
@@ -45,4 +45,70 @@ function avatar_manager_init() {
 }
 
 add_action( 'init', 'avatar_manager_init' );
+
+/**
+ * Sets up plugin defaults.
+ *
+ * @uses register_setting() For registering a setting and its sanitization callback.
+ * @uses add_settings_field() For registering a settings field to a settings page and section.
+ *
+ * @since Avatar Manager 1.0.0
+ */
+function avatar_manager_admin_init() {
+	// Registers the avatar_manager_caps setting and its sanitization callback.
+	register_setting( 'discussion', 'avatar_manager_discussion', 'avatar_manager_sanitize_options' );
+
+	// Registers the Avatar Uploads settings field under the Settings Discussion Screen.
+	add_settings_field( 'avatar-manager-avatar_uploads', __( 'Avatar Uploads', 'avatar-manager' ), 'avatar_manager_avatar_uploads_settings_field', 'discussion', 'avatars' );
+
+	// Registers the Default Size settings field under the Settings Discussion Screen.
+	add_settings_field( 'avatar-manager-default-size', __( 'Default Size', 'avatar-manager' ), 'avatar_manager_default_size_settings_field', 'discussion', 'avatars' );
+}
+
+add_action( 'admin_init', 'avatar_manager_admin_init');
+
+/**
+ *
+ */
+function avatar_manager_sanitize_options( $options ) {
+	return $options;
+}
+
+/**
+ *
+ */
+function avatar_manager_avatar_uploads_settings_field() {
+	?>
+	<fieldset>
+		<legend class="screen-reader-text">
+			<span>
+				<?php _e( 'Avatar Uploads', 'avatar-manager' ); ?>
+			</span>
+		</legend>
+		<label>
+			<input <?php checked( get_option( 'avatar_manager_avatar_uploads' ), 1, true ); ?> name="avatar_manager_avatar_uploads" type="checkbox" value="1">
+			<?php _e( 'Anyone can upload', 'avatar-manager' ); ?>
+		</label>
+	</fieldset>
+	<?php
+}
+
+/**
+ *
+ */
+function avatar_manager_default_size_settings_field() {
+	?>
+	<fieldset>
+		<legend class="screen-reader-text">
+			<span>
+				<?php _e( 'Default Size', 'avatar-manager' ); ?>
+			</span>
+		</legend>
+		<label>
+			<?php _e( 'Default size of the avatar image' ); ?>
+			<input class="small-text" min="0" name="avatar_manager_default_size" step="1" type="number" value="<?php get_option( 'avatar_manager_default_size' ); ?>">
+		</label>
+	</fieldset>
+	<?php
+}
 ?>
