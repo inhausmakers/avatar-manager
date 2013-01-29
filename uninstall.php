a8131ec8ae31f201b97852ff6ce4282e09c95970
Catalin Dogaru <catalin.dogaru@gmail.com>
2013-01-29 16:03:34 +0200

    Updated the uninstaller

--- uninstall.php
+++ uninstall.php
@@ -10,4 +10,17 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
 
 // Delete plugin options
 delete_option( 'avatar_manager' );
+
+// An associative array with criteria to match.
+$args = array(
+	'meta_key'   => 'avatar_manager_custom_avatar'
+);
+
+// Retrieves an array of users matching the criteria given in $args.
+$users = get_users( $args );
+
+foreach ( $users as $user ) {
+	// Deletes avatar image based on attachment ID.
+	avatar_manager_delete_avatar( $user->avatar_manager_custom_avatar );
+}
 ?>
