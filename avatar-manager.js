e639636fadc8fc74d0e8209fb9969021170ccbf3
Cătălin Dogaru <catalin.dogaru@gmail.com>
2013-07-09 21:22:02 +0300

    Disabled upload buttons until files are selected

--- avatar-manager.js
+++ avatar-manager.js
@@ -1,3 +1,26 @@
-jQuery( document ).ready( function() {
-	jQuery( '#your-profile' ).attr( 'enctype', 'multipart/form-data' );
-} );
+( function( $ ) {
+	$( document ).ready( function() {
+		$( '#your-profile' ).attr( 'enctype', 'multipart/form-data' );
+
+		// Disables upload buttons until files are selected
+		( function() {
+			var button, input, fieldset = $( 'fieldset' );
+
+			if ( ! fieldset.length )
+				return;
+
+			button = fieldset.find( 'input[type="submit"]' );
+			input  = fieldset.find( 'input[type="file"]' );
+
+			function toggleUploadButton() {
+				button.prop( 'disabled', '' === input.map( function() {
+					return $( this ).val();
+				} ).get().join( '' ) );
+			}
+
+			toggleUploadButton();
+
+			input.on( 'change', toggleUploadButton );
+		} )();
+	} );
+} )( jQuery );
