d0fc313201ee8173ea97f238c0e89642fa73c8e6
Cătălin Dogaru <catalin.dogaru@gmail.com>
2016-04-02 12:27:30 +0300

    Added grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,133 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  require('jit-grunt')(grunt);
+  require('time-grunt')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+      core: {
+        options: {
+          outputSourceFiles: true,
+          sourceMap: true,
+          sourceMapFilename: 'assets/css/avatar-manager.css.map',
+          sourceMapURL: 'avatar-manager.css.map',
+          strictMath: true
+        },
+        files: {
+          'assets/css/avatar-manager.css': 'less/avatar-manager.less'
+        }
+      }
+    },
+    postcss: {
+      options: {
+        map: true,
+        processors: [
+          require('autoprefixer')
+        ]
+      },
+      core: {
+        src: 'assets/css/*.css'
+      }
+    },
+    csscomb: {
+      options: {
+        config: 'less/.csscomb.json'
+      },
+      core: {
+        src: 'assets/css/avatar-manager.css',
+        dest: 'assets/css/avatar-manager.css'
+      }
+    },
+    csslint: {
+      options: {
+        csslintrc: 'less/.csslintrc'
+      },
+      core: {
+        src: 'assets/css/avatar-manager.css'
+      }
+    },
+    cssmin: {
+      options: {
+        advanced: false,
+        keepSpecialComments: '*',
+        sourceMap: true
+      },
+      core: {
+        expand: true,
+        cwd: 'assets/css',
+        src: ['*.css', '!*.min.css'],
+        dest: 'assets/css',
+        ext: '.min.css'
+      }
+    },
+    eslint: {
+      options: {
+        configFile: 'js/.eslintrc'
+      },
+      target: 'js/*.js'
+    },
+    jscs: {
+      options: {
+        config: 'js/.jscsrc'
+      },
+      grunt: {
+        src: 'Gruntfile.js'
+      },
+      core: {
+        src: 'js/*.js'
+      }
+    },
+    concat: {
+      core: {
+        src: [
+          'js/avatar-manager.js'
+        ],
+        dest: 'assets/js/avatar-manager.js'
+      }
+    },
+    uglify: {
+      options: {
+        compress: {
+          warnings: false
+        },
+        preserveComments: false
+      },
+      core: {
+        src: '<%= concat.core.dest %>',
+        dest: 'assets/js/avatar-manager.min.js'
+      }
+    },
+    watch: {
+      configFiles: {
+        options: {
+          reload: true
+        },
+        files: ['Gruntfile.js', 'package.json']
+      },
+      js: {
+        files: 'js/*.js',
+        tasks: 'js'
+      },
+      less: {
+        files: 'less/*.less',
+        tasks: 'css'
+      }
+    },
+    clean: {
+      options: {
+        force: true
+      },
+      css: 'assets/css',
+      js: 'assets/js'
+    }
+  });
+
+  grunt.registerTask('css', ['less', 'postcss', 'csscomb', 'csslint', 'cssmin']);
+  grunt.registerTask('js', ['eslint', 'jscs', 'concat', 'uglify']);
+
+  grunt.registerTask('build', ['css', 'js']);
+
+  grunt.registerTask('default', 'build');
+};
